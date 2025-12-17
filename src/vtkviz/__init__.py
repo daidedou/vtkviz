6fee6f2d0c2e5e486add50e3e75ee4c07037f8d1
daidedou <daidedou@sorpi.fr>
2025-12-17 17:18:07 +0100

    typo

--- src/vtkviz/__init__.py
+++ src/vtkviz/__init__.py
@@ -1,6 +1,6 @@
 from .renderer import VTKVisualization, VTKMultipleVizualization
 from .widgets import VTKSlider, VTKHull, VTKLine, VTKPlane
 from .vtk_surfaces import VTKSurface, VTKFile
-from .surfaces import Surface
+#from .surfaces import Surface
 from .point_cloud import VTKPointCloud, VTKPointCloudSphere, VTKVectorField
 from .utils import get_p1p2, center_mesh_give_size
